76e47b84afe4d8edd35375cdd58a440b6dc16186
wishtony <vishnutoanother@gmail.com>
2023-11-28 10:03:48 +0530

    new update

--- my-app/src/pages/UsersList.js
+++ my-app/src/pages/UsersList.js
@@ -10,6 +10,8 @@ import Footer from "../components/Footer";
 const UsersList = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -17,14 +19,15 @@ const UsersList = () => {
     const fetchUsers = async () => {
       try {
         const response = await axios.get(
-          "https://authapidemo.pythonanywhere.com/api/v1/users/",
+          `https://authapidemo.pythonanywhere.com/api/v1/users/?page=${currentPage}`,
           {
             headers: { Authorization: `Token ${token}` },
           }
         );
 
-        const data = response.data.results;
-        setUsers(data);
+        const data = response.data;
+        setUsers(data.results);
+        setTotalPages(Math.ceil(data.count / data.results.length));
       } catch (error) {
         if (error.response && error.response.status === 401) {
           console.log("Unauthorized access. Redirecting to login...");
@@ -39,7 +42,11 @@ const UsersList = () => {
     };
 
     fetchUsers();
-  }, []);
+  }, [currentPage]);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   return (
     <div>
@@ -49,30 +56,44 @@ const UsersList = () => {
         {loading ? (
           <p>Loading...</p>
         ) : (
-          <Table striped bordered hover>
-            <thead>
-              <tr className="table-head">
-                <th style={{ color: "white", width: "2%" }}>ID</th>
-
-                <th style={{ color: "white", width: "30%" }}>Name</th>
-                <th style={{ color: "white", width: "55%" }}>Username</th>
-              </tr>
-            </thead>
-            <tbody className="table-body">
-              {users.map((user) => (
-                <tr key={user.id}>
-                  <td>{user.id}</td>
-
-                  <td>{user.name}</td>
-                  <td>{user.username}</td>
+          <>
+            <Table striped bordered hover>
+              <thead>
+                <tr className="table-head">
+                  <th style={{ color: "white", width: "2%" }}>ID</th>
+                  <th style={{ color: "white", width: "30%" }}>Name</th>
+                  <th style={{ color: "white", width: "55%" }}>Username</th>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
+              </thead>
+              <tbody className="table-body">
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.username}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+            <div className="pagination-container">
+              <button
+                onClick={() => handlePageChange(currentPage - 1)}
+                disabled={currentPage === 1}
+              >
+                Previous
+              </button>
+              <span>{currentPage}</span>
+              <button
+                onClick={() => handlePageChange(currentPage + 1)}
+                disabled={currentPage === totalPages}
+              >
+                Next
+              </button>
+            </div>
+          </>
         )}
         <ToastContainer />
       </div>
-      
       <Footer />
     </div>
   );
