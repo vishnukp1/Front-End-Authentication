03d7d75ddff822f071dc5a949b00bdb7a1c62840
wishtony <vishnutoanother@gmail.com>
2023-11-28 09:52:15 +0530

    new update

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,101 +1,113 @@
-import React, { useRef } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import axios from "axios";
 import { setLogin } from "../Reducers/loginReducer";
+
+const LoginSchema = Yup.object().shape({
+  username: Yup.string().required("Username is required"),
+  upassword: Yup.string().required("Password is required"),
+});
 
 function Login() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const inputref = useRef();
-  const loginPass = async (e) => {
-    e.preventDefault();
-    const setUsername = inputref.current.username.value;
-    const setPassword = inputref.current.upassword.value;
-  
-    // Check if username or password is empty
-    if (!setUsername || !setPassword) {
-      toast.error("Username and Password are required");
-      return;
-    }
-  
+
+  const handleSubmit = async ({ username, upassword, rememberMe }, { setSubmitting }) => {
+    // Explicitly define the 'items' object
     const items = {
-      username: setUsername,
-      password: setPassword,
+      username: username,
+      password: upassword,
+      rememberMe: rememberMe,
     };
-  
+
     try {
       const response = await axios.post(
         "https://authapidemo.pythonanywhere.com/api/v1/auth/login/",
         items
       );
+
       const data = response.data;
-  
+
       if (data.token) {
-        try {
-          navigate("/usersList");
-          toast.success("SUCCESSFULLY LOGIN!");
-          // Store the token in local storage
-          localStorage.setItem("token", data.token);
-          localStorage.setItem("username", data.username);
-        } catch (error) {
-          console.error("Error in token:", error);
-        }
+        navigate("/usersList");
+        toast.success("SUCCESSFULLY LOGIN!");
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("username", data.username);
       } else {
         toast.error("Username and Password did not match");
         localStorage.clear("token");
       }
     } catch (error) {
       console.error("Error getting customer data:", error);
+      toast.error("Username and Password did not match");
+    } finally {
+      setSubmitting(false);
     }
   };
-  
 
   return (
     <div className="form-login">
-      <form ref={inputref} onSubmit={loginPass}>
-        <h3 style={{ marginTop: "1rem", textAlign: "center" }}>Sign In</h3>
-        <div className="form-text" style={{ marginTop: "1rem" }}>
-          <div className="mb-3 ">
-            <label>Username</label>
-            <input type="text" className="form-control" name="username" />
-          </div>
-          <div className="mb-3">
-            <label>Password</label>
-            <input type="password" className="form-control" name="upassword" />
-          </div>
-        </div>
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="form-btn">
-            Submit
-          </button>
-          <ToastContainer />
-        </div>
+      <Formik
+        initialValues={{
+          username: "",
+          upassword: "",
+          rememberMe: false,
+        }}
+        validationSchema={LoginSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <h3 style={{ marginTop: "1rem", textAlign: "center" }}>Sign In</h3>
+            <div className="form-text" style={{ marginTop: "1rem" }}>
+              <div className="mb-3 ">
+                <label>Username</label>
+                <Field type="text" className="form-control" name="username" />
+                <ErrorMessage name="username" component="div" className="error" />
+              </div>
+              <div className="mb-3">
+                <label>Password</label>
+                <Field type="password" className="form-control" name="upassword" />
+                <ErrorMessage name="upassword" component="div" className="error" />
+              </div>
+            </div>
+            <div className="mb-3">
+              <div className="custom-control custom-checkbox">
+                <Field
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="customCheck1"
+                  name="rememberMe"
+                />
+                <label className="custom-control-label" htmlFor="customCheck1">
+                  Remember me
+                </label>
+              </div>
+            </div>
+            <div className="d-grid">
+              <button type="submit" className="form-btn" disabled={isSubmitting}>
+                Submit
+              </button>
+              <ToastContainer />
+            </div>
 
-        <p className="forgot-password text-center mt-1">
-          Forgot <a href="forgotPassword">password?</a>
-        </p>
-        <p className="forgot-password text-center mb-1">
-          I didn't registered{" "}
-          <h6 style={{ color: "red" }} onClick={() => dispatch(setLogin())}>
-            sign Up?
-          </h6>
-        </p>
-      </form>
+            <p className="forgot-password text-center mt-1">
+              Forgot <a href="forgotPassword">password?</a>
+            </p>
+            <p className="forgot-password text-center mb-1">
+              I didn't register{" "}
+              <h6 style={{ color: "red" }} onClick={() => dispatch(setLogin())}>
+                sign Up?
+              </h6>
+            </p>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
--- my-app/src/components/SignUp.js
+++ my-app/src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import "../styles/Login.css";
 import { useDispatch } from "react-redux";
 import { setSignup } from "../Reducers/loginReducer";
@@ -9,65 +9,93 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import { toast } from "react-toastify";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 function SignUp() {
-  const inputref = useRef(null);
-  const submithandle = async () => {
-    try {
-      const Name = inputref.current.name.value;
-      const userPassword = inputref.current.password.value;
-      const userName = inputref.current.username.value;
-      if (!Name || !userName || !userPassword) {
-        toast.error("Username and Password are required");
-        return;
-      }
-
-      const item = {
-        name: Name,
-        username: userName,
-        password: userPassword,
-      };
-
-      const response = await axios.post(
-        `https://authapidemo.pythonanywhere.com/api/v1/auth/register/`,
-        item
-      );
-      let data = response.data;
-      console.log(response);
-    } catch (error) {
-      console.error("Error fetching customer data:", error);
-      // Handle the error, show an error message, etc.
-    }
-  };
-
-  console.log(inputref);
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log("You clicked submit.");
-  }
-
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      username: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Name is required"),
+      username: Yup.string().required("Username is required"),
+      password: Yup.string().required("Password is required"),
+    }),
+    onSubmit: async (values) => {
+      try {
+        const response = await axios.post(
+          "https://authapidemo.pythonanywhere.com/api/v1/auth/register/",
+          values
+        );
+        let data = response.data;
+        console.log(response);
+        navigate("/usersList");
+        // Handle success, navigate, show messages, etc.
+      } catch (error) {
+        console.error("Error fetching customer data:", error);
+        // Handle the error, show an error message, etc.
+      }
+    },
+  });
+
   return (
     <div className="form-signup">
-      <form ref={inputref} onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <h3 style={{ marginTop: "1rem", textAlign: "center" }}>Sign Up</h3>
         <div className="form-text">
           <div className="mb-3">
             <label>Name</label>
-            <input type="text" className="form-control" name="name" />
+            <input
+              type="text"
+              className="form-control"
+              name="name"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.name}
+            />
+            {formik.touched.name && formik.errors.name && (
+              <div className="error">{formik.errors.name}</div>
+            )}
           </div>
 
           <div className="mb-3">
             <label>Username</label>
-            <input type="text" className="form-control" name="username" />
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.username}
+            />
+            {formik.touched.username && formik.errors.username && (
+              <div className="error">{formik.errors.username}</div>
+            )}
           </div>
+
           <div className="mb-3">
             <label>Password</label>
-            <input type="password" className="form-control" name="password" />
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+            />
+            {formik.touched.password && formik.errors.password && (
+              <div className="error">{formik.errors.password}</div>
+            )}
           </div>
         </div>
         <div className="d-grid">
-          <button type="submit" className="form-btn" onClick={submithandle}>
+          <button type="submit" className="form-btn">
             Sign Up
           </button>
         </div>
